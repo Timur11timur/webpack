6b30c8e6ca92006fbc63ae49a312f9df2df766ea
Timur <timur@fralik.ru>
2020-12-31 11:58:54 +0200

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+      path: '/Users/timur/Sites/webpack-learning/dist',
+      filename: 'bundle.js'
+  }
+};
