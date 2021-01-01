021b7c738121813438c575751d9f5f0c1e50a2ee
Timur <timur@fralik.ru>
2021-01-02 00:43:48 +0200

    Add exporting of object

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import notifyDefault from './NotificationDefault';
 var notifyModule = require('./NotificationModule');
 import {notify, log} from './NotificationFunction';
+import notification from './NotificationObject';
 
 notifyDefault('Here is my message!');
 notifyModule('Here is my message from NotifyModule!');
 notify('Here is my message from NotifyFunction!');
 log('Here is my message from NotifyFunction!');
+notification.notify('Here is my message from NotificationObject!');
+notification.log('Here is my message from NotificationObject!');
