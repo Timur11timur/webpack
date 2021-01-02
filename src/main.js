76aba9f7e8589e8c80f68891620d81e798fb77e5
Timur <timur@fralik.ru>
2021-01-02 22:20:40 +0200

    Add babel

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ var notifyModule = require('./NotificationModule');
 import {notify, log} from './NotificationFunction';
 import notification from './NotificationObject';
 
-notifyDefault('Here is my message!');
-notifyModule('Here is my message from NotifyModule!');
-notify('Here is my message from NotifyFunction!');
-log('Here is my message from NotifyFunction!');
-notification.notify('Here is my message from NotificationObject!');
-notification.log('Here is my message from NotificationObject!');
+// notifyDefault('Here is my message!');
+// notifyModule('Here is my message from NotifyModule!');
+// notify('Here is my message from NotifyFunction!');
+// log('Here is my message from NotifyFunction!');
+// notification.notify('Here is my message from NotificationObject!');
+// notification.log('Here is my message from NotificationObject!');
+
+class Form {
+    constructor() {
+        alert('Hey everything is work!');
+    }
+}
+
+new Form();
